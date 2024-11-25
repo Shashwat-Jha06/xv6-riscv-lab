a0242b653aefcd9bbb6a515f8a0872a10b1d72cc
gdjs2 <zhaoqi.xiao@gdjs2.cn>
2024-11-25 15:57:25 -0800

    fix: the definition of thread

--- user/thread.h
+++ user/thread.h
@@ -4,7 +4,7 @@ typedef struct lock_t {
     uint locked;
 } lock_t;
 
-int thread_create(void (*start_routine)(void*), void* arg);
+int thread_create(void* (*start_routine)(void*), void* arg);
 void lock_init(struct lock_t* lk);
 void lock_acquire(struct lock_t* lk);
 void lock_release(struct lock_t* lk);
